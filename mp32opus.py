943b111e3e5fe755a10bb3f0f49c818af5dd39fe
Konstantin Batkov <batkov@gmail.com>
2018-01-02 17:49:18 +0100

    tags are mapped through decoding into flac

--- mp32opus.py
+++ mp32opus.py
@@ -54,8 +54,8 @@ def main():
     MP3 -> Opus converter
     """
     parser = argparse.ArgumentParser(description=main.__doc__, epilog="Homepage: https://github.com/kbat/mp32opus")
-    parser.add_argument('mp3', type=str, help='MP3 input file name')
-    parser.add_argument('opus', type=str, nargs='?', help="Opus output file name",default="")
+    parser.add_argument('mp3', type=str, help='MP3 input file name.')
+    parser.add_argument('opus', type=str, nargs='?', help="Opus output file name. If not specified, the MP3 file name with the .opus extension is used.",default="")
     parser.add_argument('--bitrate', dest='bitrate', type=int, help='Opus bitrate. If not specivied, set to min(48kbit/sec,half of MP3 bitrate). 48 is a good number for voice. Opus 64 corresponds to MP3 96. More details are here: https://auphonic.com/blog/2012/09/26/opus-revolutionary-open-audio-codec-podcasts-and-internet-audio', default=0, required=False)
     args = parser.parse_args()
 
@@ -70,14 +70,6 @@ def main():
     print("Bitrate: ", args.bitrate, audiofile.info.bit_rate)
     print("Duration: ", audiofile.info.time_secs)
     
-    print("artist: ", audiofile.tag.artist)
-    print("title: ", audiofile.tag.title)
-    print("album: ", audiofile.tag.album)
-    print("album_artist: ", audiofile.tag.album_artist)
-    print("track_num: ", audiofile.tag.track_num)
-    print("genre: ", audiofile.tag.genre)
-    print("year: ", audiofile.tag.getBestDate())
-
     print("Images:")
     picture = ""
     for imageinfo in audiofile.tag.images:
@@ -85,46 +77,9 @@ def main():
         img.Print()
         picture = picture + str(img)
 
-    title = audiofile.tag.title
-    if title is None:
-        title = ""
-    else:
-        title = u"--title \"%s\"" % title
 
-    artist = audiofile.tag.artist
-    if artist is None:
-        artist = ""
-    else:
-        artist = u"--artist \"%s\"" % artist
-
-    # album
-    album = audiofile.tag.album
-    if album is None:
-        album = ""
-    else:
-        album = u"--album \"%s\"" % album
-
-    # track number
-    trackn = audiofile.tag.track_num[0]
-    if trackn is None or trackn is 0:
-        trackn = ""
-    else:
-        trackn = "--comment 'TRACKNUMBER=%d'" % trackn
-
-    genre = audiofile.tag.genre
-    if genre is None:
-        genre = ""
-    else:
-        genre = u"--genre '%s'" % genre
-
-    year = audiofile.tag.getBestDate()
-    if year is None:
-        year = ""
-    else:
-        year = u"--date %s" % year
-
-    cmd = "avconv -i \"%s\" -f wav - | opusenc --bitrate %.3f %s %s %s %s %s %s %s - \"%s\"" % (args.mp3, args.bitrate, artist, title, album, genre, year, trackn, picture, args.opus)
-# This maps the tags automatically but unstable:
+    cmd = "avconv -i \"%s\" -f flac - | opusenc --bitrate %.3f %s - \"%s\"" % (args.mp3, args.bitrate, picture, args.opus)
+# This looks more elegant, but it's yet unstable with current avconv:
 #    cmd = "avconv -i %s -map 0:a -codec:a opus -b:a %dk -vbr on -strict -2 %s" % (args.mp3, args.bitrate, args.opus)
     print(cmd)
 
